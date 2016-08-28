1dac9c6a4659d16cd675442bf7f1964659fa888c
Mafrans K <malte.kluft@gmail.com>
2016-08-28 20:31:33 +0200

    Add maintenance mode.

---
+++ src/com/Shadersoft/UniverseMG/Commands/Command_maintenancemode.java
@@ -0,0 +1,54 @@
+package com.Shadersoft.UniverseMG.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import com.Shadersoft.UniverseMG.Messages;
+import com.Shadersoft.UniverseMG.Ranks.Rank;
+import com.Shadersoft.UniverseMG.UniverseMG;
+
+public class Command_maintenancemode implements UMGCommand
+{
+    public UniverseMG plugin = UniverseMG.plugin;
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    {
+        if(Rank.getSenderRank(sender).getPriority() < this.getRank().getPriority())
+        {
+            sender.sendMessage(Messages.MSG_NO_PERMS);
+
+            return true;
+        }
+        
+        if(args.length != 1)
+        {
+            return false;
+        }
+        switch(args[0].toUpperCase())
+        {
+            case "ON":
+            {
+                plugin.config.set("maintenance_mode", true);
+                plugin.saveConfig();
+                return true;
+            }
+            case "OFF":
+            {
+                plugin.config.set("maintenance_mode", false);
+                plugin.saveConfig();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Rank getRank()
+    {
+        return Rank.ADMIN;
+    }
+}
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
--- src/com/Shadersoft/UniverseMG/Handlers/PlayerHandler.java
+++ src/com/Shadersoft/UniverseMG/Handlers/PlayerHandler.java
@@ -6,6 +6,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 
 import com.Shadersoft.UniverseMG.Messages;
+import com.Shadersoft.UniverseMG.Ranks.Rank;
+import com.Shadersoft.UniverseMG.Ranks.RankType;
 import com.Shadersoft.UniverseMG.UniverseMG;
 
 public class PlayerHandler implements Listener
@@ -27,6 +29,14 @@ public class PlayerHandler implements Listener
             event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_BANNED,
                            Messages.MOD_TAG + "You are banned from this server for "
                            + this.plugin.banconfig.getConfig().getString(player));
+        }
+        
+        if(plugin.config.getBoolean("maintenance_mode"))
+        {
+            if(Rank.getRankFromName(event.getName()).getType() != RankType.STAFF)
+            {
+                event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, Messages.MOD_TAG + "Server is currently in maintenance, only staff may join.");
+            }
         }
     }
 }
